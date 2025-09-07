819c304a5da7a947e0e637d629f8f3e92d9944f4
GetBoros <getboros@gmail.com>
2025-09-07 11:42:48 +0300

    chore: .clang-format for code style consistency

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include "Config.hpp"
 #include <raylib.h>
+
+#include "Config.hpp"
 
 Font Init_Load_Font_Ex()
 {
@@ -7,21 +8,17 @@ Font Init_Load_Font_Ex()
     int codepoints[256];
     int count = 0;
 
-    codepoints[count++] = 32; // Пробел
+    codepoints[count++] = 32;  // Пробел
 
-    for (int i = 48; i <= 57; ++i)
-        codepoints[count++] = i; // Цифры 0-9
-    for (int i = 65; i <= 90; ++i)
-        codepoints[count++] = i; // A-Z
-    for (int i = 97; i <= 122; ++i)
-        codepoints[count++] = i; // a-z
-    for (int i = 1040; i <= 1103; ++i)
-        codepoints[count++] = i; // А-я
+    for (int i = 48; i <= 57; ++i) codepoints[count++] = i;      // Цифры 0-9
+    for (int i = 65; i <= 90; ++i) codepoints[count++] = i;      // A-Z
+    for (int i = 97; i <= 122; ++i) codepoints[count++] = i;     // a-z
+    for (int i = 1040; i <= 1103; ++i) codepoints[count++] = i;  // А-я
 
-    codepoints[count++] = 1025; // Ё
-    codepoints[count++] = 1105; // ё
-    codepoints[count++] = 33; // !
-    codepoints[count++] = 46; // .
+    codepoints[count++] = 1025;  // Ё
+    codepoints[count++] = 1105;  // ё
+    codepoints[count++] = 33;    // !
+    codepoints[count++] = 46;    // .
 
     return LoadFontEx(fontPath, 96, codepoints, count);
 }
@@ -29,44 +26,49 @@ Font Init_Load_Font_Ex()
 int main()
 {
     bool isDragging = false;
+    int *ptr = 0;
     const int screenWidth = 800;
     const int screenHeight = 450;
-    Vector2 dragOffset = { 0.0f, 0.0f };
+    Vector2 dragOffset = {0.0f, 0.0f};
 
     SetConfigFlags(FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_UNDECORATED);
     InitWindow(screenWidth, screenHeight, "GBLab - Плавное прозрачное окно!");
     SetWindowPosition(0, 0);
 
+    if (ptr != 0)
+    {
+        /* code */
+    }
+
     SetTargetFPS(60);
 
-    
     Font cyrillicFont = Init_Load_Font_Ex();
 
-    while (!WindowShouldClose() )
+    while (!WindowShouldClose())
     {
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) )
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
             isDragging = true;
-            
+
             dragOffset.x = GetMousePosition().x;
             dragOffset.y = GetMousePosition().y;
         }
 
-        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) )
-            isDragging = false;
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) isDragging = false;
 
         if (isDragging)
         {
             Vector2 currentMousePos = GetMousePosition();
             Vector2 windowPos = GetWindowPosition();
 
-            SetWindowPosition(windowPos.x + currentMousePos.x - dragOffset.x, windowPos.y + currentMousePos.y - dragOffset.y);
+            SetWindowPosition(windowPos.x + currentMousePos.x - dragOffset.x,
+                              windowPos.y + currentMousePos.y - dragOffset.y);
         }
 
         BeginDrawing();
 
         ClearBackground(BLANK);
-        
+
         DrawTextEx(cyrillicFont, "Теперь точно работает как надо!", {190, 200}, 20, 1, LIME);
         DrawTextEx(cyrillicFont, "Никакого дрожания и убегания.", {240, 230}, 16, 1, GRAY);
 
